4a9451ce1342499830f47bc14710ca092c09ab54
Rod Maxwell <rodmxw11@gmail.com>
2021-11-30 07:12:12 -0500

    Fix scripts/*.sh

--- scripts/extract-text.sh
+++ scripts/extract-text.sh
@@ -2,8 +2,8 @@
 
 # See: https://pdfbox.apache.org/2.0/commandline.html
 
-pdfbox=pdfbox-app-2.0.24.jar
+pdfbox=../lib/pdfbox-app-2.0.24.jar
 flags=
-inputfile=../Amazon-Card/2021/20211104-statements-4220-.pdf
-java -jar $pdfbox ExtractText $flags $inputfile text.OUT
+inputfile=../data/Amazon-Card/2021/20211104-statements-4220-.pdf
+java -jar $pdfbox ExtractText $flags $inputfile ../workspace/text.OUT
 
--- scripts/pdf-debugger.sh
+++ scripts/pdf-debugger.sh
@@ -2,8 +2,8 @@
 
 # See: https://pdfbox.apache.org/2.0/commandline.html
 
-pdfbox=pdfbox-app-2.0.24.jar
+pdfbox=../lib/pdfbox-app-2.0.24.jar
 flags=
-inputfile=../Amazon-Card/2021/20211104-statements-4220-.pdf
+inputfile=../data/Amazon-Card/2021/20211104-statements-4220-.pdf
 java -jar $pdfbox PDFDebugger $flags $inputfile
 
