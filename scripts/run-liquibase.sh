1a309ffd21701d3dc3861c3d3b939b95c9292710
Rod Maxwell <rodmxw11@gmail.com>
2021-12-01 20:00:31 -0500

    liquibase works OK

--- scripts/run-liquibase.sh
+++ scripts/run-liquibase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-liquibase=~/JavaPrograms/liquibase-4.6.1
+liquibase=~/JavaPrograms/liquibase-4.6.1/
 PATH=$liquibase:$PATH
 
 liquibase --defaultsFile=../src/database-config/liquibase.properties $*
